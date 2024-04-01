8734de85db4b5547b13a2dee0cfee2dff8ad3383
nmtri2810 <nmtri281010@gmail.com>
2024-04-02 02:31:17 +0700

    Add redux combos

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,34 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  css: {
-    devSourcemap: true
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), svgr()],
+    server: {
+      port: parseInt(env.VITE_PORT, 10)
+    },
+    css: {
+      devSourcemap: true
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/setupTest.ts'],
+      coverage: {
+        include: ['src/**']
+      }
     }
   }
 })
