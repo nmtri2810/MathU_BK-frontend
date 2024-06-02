964612ea57a36b6f197bbf022a3e74c3a92bed71
nmtri2810 <nmtri281010@gmail.com>
2024-06-02 14:50:55 +0700

    fix: fix avatar dropdown

--- src/components/pages/layout/avatarDropdown.tsx
+++ src/components/pages/layout/avatarDropdown.tsx
@@ -3,16 +3,10 @@ import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { logoutRequest } from '@/store/actions/auth';
 import { useNavigate } from 'react-router-dom';
 import AvatarFull from '@/components/common/avatarFull';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
 import { LogOut, Settings, User } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { NavItem } from './navbarItems';
+import { Button } from '@/components/ui/button';
 
 const AvatarDropdown: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -25,36 +19,41 @@ const AvatarDropdown: React.FC = () => {
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className='flex h-full items-center rounded-sm px-4 font-normal hover:bg-accent hover:text-accent-foreground focus-visible:outline-none'>
+    <Popover>
+      <PopoverTrigger className='flex h-full items-center rounded-sm px-4 font-normal hover:bg-accent hover:text-accent-foreground focus-visible:outline-none'>
         <AvatarFull className='size-8' />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end' className='min-w-56'>
-        <DropdownMenuLabel>{user?.email}</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+      </PopoverTrigger>
+      <PopoverContent align='end' className='w-fit p-1'>
+        <div className='flex items-center gap-2 px-3 py-2'>
+          <AvatarFull className='size-9' />
+          <div>
+            <p className='text-sm font-semibold'>{user?.username} Username</p>
+            <p className='text-sm text-gray-500'>{user?.email}</p>
+          </div>
+        </div>
+        <div className='-mx-1 my-1 h-px bg-muted'></div>
 
-        <DropdownMenuItem disabled>Role: {user?.role.id}</DropdownMenuItem>
-        <DropdownMenuItem disabled>Username: {user?.username}</DropdownMenuItem>
-        <DropdownMenuSeparator />
+        {/* temp */}
+        {/* <p>Role: {user?.role.id}</p> */}
 
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <User className='mr-2 h-4 w-4' />
+        <div>
+          <NavItem className='justify-start rounded-sm px-3 py-2' link=''>
+            <User className='mr-2 size-4' />
             <span>Profile</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className='mr-2 h-4 w-4' />
+          </NavItem>
+          <NavItem className='justify-start rounded-sm px-3 py-2' link=''>
+            <Settings className='mr-2 size-4' />
             <span>Settings</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
+          </NavItem>
+        </div>
+        <div className='-mx-1 my-1 h-px bg-muted'></div>
 
-        <DropdownMenuItem onClick={handleLogout}>
-          <LogOut className='mr-2 h-4 w-4' />
+        <Button variant='ghost' className='w-full justify-start rounded-sm px-3' onClick={handleLogout}>
+          <LogOut className='mr-2 size-4' />
           <span>Log out</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </Button>
+      </PopoverContent>
+    </Popover>
   );
 };
 
